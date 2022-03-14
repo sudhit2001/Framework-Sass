8f1c8f2f74d5b4f0e58442794592afff194e62b0
sudhit2001 <sudhit21soni@gmail.com>
2022-03-14 21:48:03 +0530

    Initial Commit

---
+++ prac/app.js
@@ -0,0 +1,6 @@
+// var a=require('./index.js')(10);
+// console.log(a);
+var {a,b,c}=require('./index.js');
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ prac/index.js
@@ -0,0 +1,8 @@
+// var a=function(n){
+//     return n;
+// }
+module.exports={
+    a:10,
+    b:20,
+    c:30
+};
